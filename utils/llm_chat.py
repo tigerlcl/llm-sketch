04158a026411492474a273f881103b4bb068597f
Tiger Li <tiger@mailtime.com>
2024-04-03 14:48:32 +0800

    minor update

---
+++ utils/llm_chat.py
@@ -0,0 +1,15 @@
+
+import config
+from openai import OpenAI
+
+client = OpenAI()
+
+completion = client.chat.completions.create(
+  model="gpt-3.5-turbo",
+  messages=[
+    {"role": "system", "content": "You are a helpful assistant."},
+    {"role": "user", "content": "Hello!"}
+  ]
+)
+
+print(completion.choices[0].message)
